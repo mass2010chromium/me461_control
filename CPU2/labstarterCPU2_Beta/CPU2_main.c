00e8ef98f0751ed54f0c1b90c4abe7d300b034c4
DanBlock <d-block@tyler.com>
2021-11-23 13:30:41 -0600

    Added CPU2 code

---
+++ CPU2/labstarterCPU2_Beta/CPU2_main.c
@@ -0,0 +1,192 @@
+//#############################################################################
+// FILE:   labstarter_main.c
+//
+// TITLE:  Lab Starter
+//#############################################################################
+
+// Included Files
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <math.h>
+#include <limits.h>
+#include "F28x_Project.h"
+#include "driverlib.h"
+#include "device.h"
+
+#include "dsp.h"
+#include "fpu32/fpu_rfft.h"
+
+#define PI          3.1415926535897932384626433832795
+#define TWOPI       6.283185307179586476925286766559
+#define HALFPI      1.5707963267948966192313216916398
+
+
+// Interrupt Service Routines predefinition
+__interrupt void cpu_timer0_isr(void);
+__interrupt void cpu_timer1_isr(void);
+__interrupt void cpu_timer2_isr(void);
+__interrupt void SWI_isr(void);
+
+// Count variables
+uint32_t numTimer0calls = 0;
+uint32_t numSWIcalls = 0;
+
+void main(void)
+{
+    // PLL, WatchDog, enable Peripheral Clocks
+    // This example function is found in the F2837xD_SysCtrl.c file.
+    InitSysCtrl();
+
+    // Clear all interrupts and initialize PIE vector table:
+    // Disable CPU interrupts
+    DINT;
+
+    // Initialize the PIE control registers to their default state.
+    // The default state is all PIE interrupts disabled and flags
+    // are cleared.
+    // This function is found in the F2837xD_PieCtrl.c file.
+    InitPieCtrl();
+
+    // Disable CPU interrupts and clear all CPU interrupt flags:
+    IER = 0x0000;
+    IFR = 0x0000;
+
+    // Initialize the PIE vector table with pointers to the shell Interrupt
+    // Service Routines (ISR).
+    // This will populate the entire table, even if the interrupt
+    // is not used in this example.  This is useful for debug purposes.
+    // The shell ISR routines are found in F2837xD_DefaultIsr.c.
+    // This function is found in F2837xD_PieVect.c.
+    InitPieVectTable();
+
+    // Interrupts that are used in this example are re-mapped to
+    // ISR functions found within this project
+    EALLOW;  // This is needed to write to EALLOW protected registers
+    PieVectTable.TIMER0_INT = &cpu_timer0_isr;
+    PieVectTable.TIMER1_INT = &cpu_timer1_isr;
+    PieVectTable.TIMER2_INT = &cpu_timer2_isr;
+    PieVectTable.EMIF_ERROR_INT = &SWI_isr;
+    EDIS;    // This is needed to disable write to EALLOW protected registers
+
+
+    // Initialize the CpuTimers Device Peripheral. This function can be
+    // found in F2837xD_CpuTimers.c
+    InitCpuTimers();
+
+    // Configure CPU-Timer 0, 1, and 2 to interrupt every second:
+    // 200MHz CPU Freq, 1 second Period (in uSeconds)
+    ConfigCpuTimer(&CpuTimer0, 200, 200000); // 200ms
+    ConfigCpuTimer(&CpuTimer1, 200, 20000);
+    ConfigCpuTimer(&CpuTimer2, 200, 40000);
+
+    // Enable CpuTimer Interrupt bit TIE
+    CpuTimer0Regs.TCR.all = 0x4000;
+    CpuTimer1Regs.TCR.all = 0x4000;
+    CpuTimer2Regs.TCR.all = 0x4000;
+
+    // Enable CPU int1 which is connected to CPU-Timer 0, CPU int13
+    // which is connected to CPU-Timer 1, and CPU int 14, which is connected
+    // to CPU-Timer 2:  int 12 is for the SWI.  
+    IER |= M_INT1;
+    IER |= M_INT12;
+    IER |= M_INT13;
+    IER |= M_INT14;
+
+    // Enable TINT0 in the PIE: Group 1 interrupt 7
+    PieCtrlRegs.PIEIER1.bit.INTx7 = 1;
+
+	// Enable SWI in the PIE: Group 12 interrupt 9
+    PieCtrlRegs.PIEIER12.bit.INTx9 = 1;
+
+
+    // Need to perform these kind of steps for each peripheral that CPU2 needs to control
+//    //
+//    // Transfer ownership of EPWM1 and ADCA to CPU02
+//    //
+//    EALLOW;
+//    DevCfgRegs.CPUSEL0.bit.EPWM1 = 1;
+//    DevCfgRegs.CPUSEL11.bit.ADC_A = 1;
+//    EDIS;
+
+
+
+
+
+    // Enable global Interrupts and higher priority real-time debug events
+    EINT;  // Enable Global interrupt INTM
+    ERTM;  // Enable Global realtime interrupt DBGM
+
+    
+    // IDLE loop. Just sit and loop forever (optional):
+    while(1)
+    {
+
+    }
+
+}
+
+
+// SWI_isr,  Using this interrupt as a Software started interrupt
+__interrupt void SWI_isr(void) {
+
+    // These three lines of code allow SWI_isr, to be interrupted by other interrupt functions
+	// making it lower priority than all other Hardware interrupts.  
+	PieCtrlRegs.PIEACK.all = PIEACK_GROUP12;
+    asm("       NOP");                    // Wait one cycle
+    EINT;                                 // Clear INTM to enable interrupts
+	
+	
+	
+    // Insert SWI ISR Code here.......
+	
+	
+    numSWIcalls++;
+    
+    DINT;
+
+}
+
+// cpu_timer0_isr - CPU Timer0 ISR
+__interrupt void cpu_timer0_isr(void)
+{
+    CpuTimer0.InterruptCount++;
+
+    numTimer0calls++;
+
+//    if ((numTimer0calls%50) == 0) {
+//        PieCtrlRegs.PIEIFR12.bit.INTx9 = 1;  // Manually cause the interrupt for the SWI
+//    }
+
+    GpioDataRegs.GPBTOGGLE.bit.GPIO34 = 1;
+
+    // Acknowledge this interrupt to receive more interrupts from group 1
+    PieCtrlRegs.PIEACK.all = PIEACK_GROUP1;
+}
+
+// cpu_timer1_isr - CPU Timer1 ISR
+__interrupt void cpu_timer1_isr(void)
+{
+    CpuTimer1.InterruptCount++;
+}
+
+// cpu_timer2_isr CPU Timer2 ISR
+__interrupt void cpu_timer2_isr(void)
+{
+    CpuTimer2.InterruptCount++;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
